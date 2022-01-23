1572ae99b73fe8df481a019d4e9cf8063e1cdac6
ADM <darkgaroto@gmail.com>
2022-01-23 11:25:52 -0300

    :sparkles: New Commands

--- main.py
+++ main.py
@@ -1,27 +1,66 @@
+from distutils import command
 import os
 from tkinter import *
 import tkinter.filedialog
 
 class Commands:
   def saveas():
-    global text
-    t = text.get('1.0', 'end-1c')
-    savelocation = tkinter.filedialog.asksaveasfilename()
-    file1 = open(savelocation, "w+")
-    file1.write(t)
-    file1.close()
+    global file
+    try:
+      content = str(text_main.get(1.0, tkinter.END))
+      file = tkinter.filedialog.asksaveasfile(mode='w', defaultextension='.txt', filetypes=(('Text files', '*.txt'), ('all files', '*.*')))
+      file.write(content)
+      file.close()
 
-root = Tk()
-text = Text(root)
-text.grid()
-#root.geometry('1200x800')
-root.title('MrNotepad - Editor de Texto')
+    except:
+      return  
+  
+  def save():
+    global file
+    try:
+      if file:
+        content = str(text_main.get(1.0, tkinter.END))
+        with open(file, "w", encoding="utf-8") as fw:
+          fw.write(content)
+      
+      else:
+        file = tkinter.filedialog.asksaveasfile(mode="w", defaultextension='.txt', filetypes=(('Text file', '*.txt'), ('all files', '*.*')))
+        content2 = text_main.get(1.0, tkinter.END)
+        file.write(content2)
+        file.close()
+
+    except:
+      return
+  
+  def open_file():
+    global file
+    file = tkinter.filedialog.askopenfile(initialdir=os.getcwd(), title='Select file', filetypes=(('Text file', '*.txt'), ('All files', '*.*')))
+    with open(file, "r") as fr:
+        text_main.delete(1.0, tkinter.END)
+        text_main.insert(1.0, fr.read())
+    
+    root.title(os.path.basename(file))
+
+  
+  def new_file():
+    global file
+    file=""
+    new_window = Toplevel(root)
+    new_window.title('MrNotepad - New file')
+    new_text = tkinter.Text(new_window)
+    new_text.grid(column=0, row=1, sticky='w', padx=10, pady=10)
+    #text_main.delete(1.0, tkinter.END)
+
+root = tkinter.Tk()
+text_main = tkinter.Text(root)
+text_main.grid(column=0, row=1, sticky='w', padx=10, pady=10)
+root.title('MrNotepad - Text Editor')
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=False)
-filemenu.add_command(label='New')
-filemenu.add_command(label='Open')
-filemenu.add_command(label='Save')
+filemenu.add_command(label='New', command=Commands.new_file)
+filemenu.add_command(label='Open', command=Commands.open_file)
+filemenu.add_command(label='Save', command=Commands.save)
 filemenu.add_command(label='Save as...', command=Commands.saveas)
 
 filemenu.add_separator()
